141c9708a4e1c5da2916483c7ff825be6f48d798
Evan Williams <evanawilliams@live.com>
2022-01-08 15:26:26 +1100

    😊

--- run_leak_check.sh
+++ run_leak_check.sh
@@ -1,3 +1,5 @@
 cd ~/dev/gc_rs
 cargo build --bin leak_check
 valgrind -q --leak-check=full ~/dev/gc_rs/target/debug/leak_check
+cargo build --release --bin leak_check
+valgrind -q --leak-check=full ~/dev/gc_rs/target/release/leak_check
